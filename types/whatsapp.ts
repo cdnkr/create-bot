ec0bb37722f4b3d003c1768fedcfe5bf5c8bda68
cdnkr <chad@forgood.co.za>
2024-09-03 13:21:44 +0200

    adds WhatsApp message types based on WhatsApp Cloud API message types

--- types/whatsapp.ts
+++ types/whatsapp.ts
@@ -1,19 +1,147 @@
-export type WhatsAppMessage = ({
-    msg: string;
-    time: string;
-    sent: boolean;
-    isLink?: undefined;
-    img?: undefined;
-} | {
-    msg: string;
-    isLink: boolean;
-    time: string;
-    sent: boolean;
-    img?: undefined;
-} | {
-    img: string;
-    time: string;
-    sent: boolean;
-    msg?: undefined;
-    isLink?: undefined;
-});
+export type WhatsAppMessageType = 
+    | WhatsAppTextMessage
+    | WhatsAppImageMessage
+    | WhatsAppDocumentMessage
+    | WhatsAppLocationMessage
+    | WhatsAppVideoMessage
+    | WhatsAppInteractiveButtonMessage
+    | WhatsAppInteractiveCtaUrlMessage
+    | WhatsAppInteractiveListMessage;
+
+export interface WhatsAppBaseMessage {
+    // For use in demo/editor
+    time?: string;
+    isBot?: boolean;
+    // WA specific
+    messaging_product?: "whatsapp";
+    recipient_type?: "individual";
+    to?: string;
+    type: string;
+}
+
+export interface WhatsAppTextMessage extends WhatsAppBaseMessage {
+    type: "text";
+    text: {
+        preview_url: boolean;
+        body: string;
+    };
+}
+
+export interface WhatsAppImageMessage extends WhatsAppBaseMessage {
+    type: "image";
+    image: {
+        id?: string;
+        link?: string;
+        caption?: string;
+    };
+}
+
+export interface WhatsAppDocumentMessage extends WhatsAppBaseMessage {
+    type: "document";
+    document: {
+        id?: string;
+        link?: string;
+        caption?: string;
+        filename?: string;
+    };
+}
+
+export interface WhatsAppLocationMessage extends WhatsAppBaseMessage {
+    type: "location";
+    location: {
+        latitude: string;
+        longitude: string;
+        name?: string;
+        address?: string;
+    };
+}
+
+export interface WhatsAppVideoMessage extends WhatsAppBaseMessage {
+    type: "video";
+    video: {
+        id?: string;
+        link?: string;
+        caption?: string;
+    };
+}
+
+export interface WhatsAppInteractiveButtonMessage extends WhatsAppBaseMessage {
+    type: "interactive";
+    interactive: {
+        type: "button";
+        header?: {
+            type: "text" | "image";
+            text?: string;
+            image?: {
+                id: string;
+            };
+        };
+        body: {
+            text: string;
+        };
+        footer?: {
+            text: string;
+        };
+        action: {
+            buttons: WhatsAppMessageReplyButton[];
+        };
+    };
+}
+
+export interface WhatsAppInteractiveCtaUrlMessage extends WhatsAppBaseMessage {
+    type: "interactive";
+    interactive: {
+        type: "cta_url";
+        header?: {
+            type: "text";
+            text: string;
+        };
+        body?: {
+            text: string;
+        };
+        footer?: {
+            text: string;
+        };
+        action: {
+            name: "cta_url";
+            parameters: {
+                display_text: string;
+                url: string;
+            };
+        };
+    };
+}
+
+export interface WhatsAppInteractiveListMessage extends WhatsAppBaseMessage {
+    type: "interactive";
+    interactive: {
+        type: "button";
+        header?: {
+            type: "text" | "image";
+            text?: string;
+            image?: {
+                id: string;
+            };
+        };
+        body: {
+            text: string;
+        };
+        footer?: {
+            text: string;
+        };
+        action: {
+            buttons: WhatsAppMessageReplyButton[];
+        };
+    };
+}
+
+
+export interface WhatsAppMessageReplyButton {
+    type: "reply";
+    reply: WhatsAppMessageReply;
+}
+
+export interface WhatsAppMessageReply {
+    id: string;
+    title: string;
+}
