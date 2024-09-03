f789384d3c99610d7ef40a9fb18297ca623b17f7
Chad Danker <chaddanker@gmail.com>
2024-09-04 00:50:41 +0200

    removes unnecessary margins on button component

--- components/general/button.tsx
+++ components/general/button.tsx
@@ -16,7 +16,7 @@ function Button({
     return (
         <button
             onClick={onClick}
-            className={`text-white mt-2 shrink-0 flex-nowrap text-nowrap bg-black focus:ring-orange-300 ${className} hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ${disabled ? 'opacity-50' : ''}`}
+            className={`text-white shrink-0 flex-nowrap text-nowrap bg-black focus:ring-orange-300 ${className} hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ${disabled ? 'opacity-50' : ''}`}
             disabled={disabled}
         >
             {text}
