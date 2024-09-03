eaf2a32ff70f951a8b620e15749eec78667bf226
Chad Danker <chaddanker@gmail.com>
2024-09-04 01:52:23 +0200

    adds support for labels on input component

--- components/general/input.tsx
+++ components/general/input.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Label from './label';
 
 interface Props {
     value?: string;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
     icon?: React.ReactElement;
+    label?: string;
     className?: string;
     placeholder?: string;
 }
@@ -12,24 +14,28 @@ function Input({
     value,
     onChange,
     icon,
-    className,
+    label = '',
+    className = '',
     placeholder = ''
 }: Props) {
 
     return (
-        <div className='relative'>
-            {icon && (
-                <div className='absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none'>
-                    {icon}
-                </div>
-            )}
-            <input
-                value={value}
-                onChange={onChange}
-                type='text'
-                className={`${className} ${icon ? 'ps-12' : ''} 'rounded-full p-2.5 rounded-lg w-full text-base md:text-sm block bg-gray-100 placeholder-gray-400  border-transparent border-solid border-4 focus:border-orange-300 focus:outline-none`}
-                placeholder={placeholder}
-            />
+        <div className='w-full'>
+            {label && <Label label={label} />}
+            <div className='relative'>
+                {icon && (
+                    <div className='absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none'>
+                        {icon}
+                    </div>
+                )}
+                <input
+                    value={value}
+                    onChange={onChange}
+                    type='text'
+                    className={`${className} ${icon ? 'ps-12' : ''} 'rounded-full p-2.5 rounded-lg w-full text-base md:text-sm block bg-gray-100 placeholder-gray-400  border-transparent border-solid border-4 focus:border-orange-300 focus:outline-none`}
+                    placeholder={placeholder}
+                />
+            </div>
         </div>
     )
 }
