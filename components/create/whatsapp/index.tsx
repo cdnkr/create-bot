a877ab88839b67d917c291f6aad296d608d3f361
Chad Danker <chaddanker@gmail.com>
2024-09-04 00:48:29 +0200

    sets initial messages with empty array

--- components/create/whatsapp/index.tsx
+++ components/create/whatsapp/index.tsx
@@ -2,11 +2,12 @@
 
 import WhatsAppChatDemo from "@/components/demo/whatsapp";
 import Sidebar from "./sidebar";
-import { sampleConversation } from "@/data/sample-conversation";
+// import { sampleConversation } from "@/data/sample-conversation";
 import { useState } from "react";
+import { WhatsAppMessageType } from "@/types/whatsapp";
 
 export default function BuildWhatsAppBot() {
-  const [initialMessages, setInitialMessages] = useState([...sampleConversation]);
+  const [initialMessages, setInitialMessages] = useState<WhatsAppMessageType[]>([]);
 
   return (
     <div className="w-full flex flex-col md:flex-row gap-5">
