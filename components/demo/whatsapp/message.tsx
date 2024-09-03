37234c5a7dbbb202d7e354decb5b4cb38e08bf2b
cdnkr <chad@forgood.co.za>
2024-09-03 17:40:56 +0200

    adds support for showing interactive WA list messages in demo

--- components/demo/whatsapp/message.tsx
+++ components/demo/whatsapp/message.tsx
@@ -1,5 +1,6 @@
 import { WhatsAppMessageType } from "@/types/whatsapp";
 import { isLink } from "@/utils/url";
+import { IoListOutline } from "react-icons/io5";
 import React from "react";
 
 interface Props {
@@ -66,15 +67,52 @@ function Message({ message }: Props) {
           className="flex justify-between items-end max-w-[410px] p-2"
           style={{ wordBreak: "break-word" }}
         >
-          {message.type === 'text' && (
-            <>
-              {formatMessageText(message.text.body)}
-            </>
-          )}
+          {formatMessageText(message.text.body)}
           <p className="text-[#8796a1] text-[10px] min-w-[50px]">{message.time}</p>
         </div>
       )}
-      
+
+      {/* Interactive messages */}
+      {(message.type === 'interactive') && (
+        <div className="w-full flex flex-col">
+          <div
+            className="flex justify-between items-end max-w-[410px] p-2"
+            style={{ wordBreak: "break-word" }}
+          >
+            <div className="w-full flex flex-col gap-2 mb-1">
+              {(message.interactive.header?.type === 'text') ? (
+                <p className="text-white whitespace-pre-line text-base font-bold mr-2 inline">
+                  {message.interactive.header.text}
+                </p>
+              ) : (message.interactive.header?.type === 'image') && (
+                <div className="relative w-100 p-2">
+                  {message.interactive.header?.image?.link && (
+                    <img
+                      src={message.interactive.header.image.link}
+                      alt="img_message"
+                      className="rounded-md max-w-[270px] w-100"
+                    />
+                  )}
+                </div>
+              )}
+              {message.interactive.body?.text && formatMessageText(message.interactive.body.text)}
+            </div>
+            <p className="text-[#8796a1] text-[10px] min-w-[50px]">{message.time}</p>
+          </div>
+          {message.interactive?.action?.button && (
+            <>
+              <hr className="w-full" />
+              <div className="w-full p-3 gap-1.5 text-sm text-green-500 flex justify-center items-center cursor-pointer">
+                <div className="text-[22px]">
+                  <IoListOutline />
+                </div>
+                {message.interactive.action.button}
+              </div>
+            </>
+          )}
+        </div>
+      )}
+
     </div>
   );
 }
