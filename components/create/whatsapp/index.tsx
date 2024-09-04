a8d930900e411d0b11a86da4ee324ca6de91da99
Chad Danker <chaddanker@gmail.com>
2024-09-04 23:42:37 +0200

    removes sidebar from WA chat demo page

--- components/create/whatsapp/index.tsx
+++ components/create/whatsapp/index.tsx
@@ -1,22 +1,14 @@
 'use client'
 
 import WhatsAppChatDemo from "@/components/demo/whatsapp";
-import Sidebar from "./sidebar";
-// import { sampleConversation } from "@/data/sample-conversation";
-import { useState } from "react";
 import { WhatsAppMessageType } from "@/types/whatsapp";
+import { useState } from "react";
 
 export default function BuildWhatsAppBot() {
   const [initialMessages, setInitialMessages] = useState<WhatsAppMessageType[]>([]);
 
   return (
     <div className="w-full flex flex-col md:flex-row gap-5">
-      <div className="w-full md:w-1/2">
-        <Sidebar 
-          messages={initialMessages}
-          setMessages={setInitialMessages} 
-        />
-      </div>
       <div className="w-full">
         <WhatsAppChatDemo initialMessages={initialMessages} />
       </div>
