2ee3ca36f71d2fccdf15625c09087d26348df7f4
Chad Danker <chaddanker@gmail.com>
2024-09-04 23:41:52 +0200

    refactors add WA message component logic to hook

--- components/create/whatsapp/add-template.tsx
+++ components/create/whatsapp/add-template.tsx
@@ -4,11 +4,10 @@ import Input from '@/components/general/input';
 import Label from '@/components/general/label';
 import Modal from '@/components/general/modal';
 import messageTypeInitializers from '@/data/whatsapp/add-message-initializers';
-import { getNestedValue, setNestedValue, useNestedState } from '@/hooks/useNestedState';
+import useAddWhatsAppMessage from '@/hooks/useAddWhatsAppMessage';
 import { WhatsAppMessageType } from '@/types/whatsapp';
-import { deepClone } from '@/utils/object';
-import { camelCaseToText, capitalize } from '@/utils/text';
-import { ChangeEvent, useEffect, useRef, useState } from 'react';
+import { camelCaseToText } from '@/utils/text';
+import { ChangeEvent } from 'react';
 
 interface Props {
     modalTitle: string;
@@ -19,100 +18,17 @@ interface Props {
 }
 
 function AddTemplate({ modalTitle, showModal, setShowModal, messages, setMessages }: Props) {
-    const [templateName, setTemplateName] = useState('');
-    const initializerRef = useRef<WhatsAppMessageType>(messageTypeInitializers.text);
-    const { state, getNestedState, setNestedState, resetState } = useNestedState(messageTypeInitializers.text);
+    const {
+        state,
+        templateName,
+        setTemplateName,
+        reInitialize,
+        renderInputs
+    } = useAddWhatsAppMessage('text');
 
     function onAddClick() {
         setMessages([...messages, state]);
         setShowModal(false);
-    }
-
-    function reInitialize(key: string) {
-        if (messageTypeInitializers[key]) {
-            initializerRef.current = deepClone(messageTypeInitializers[key]);
-            resetState(deepClone(messageTypeInitializers[key]));
-        }
-    }
-
-    const shouldRenderInput = (keys: string[]): boolean => {
-        if (!initializerRef.current) return false;
-        // @ts-ignore
-        return getNestedValue(initializerRef.current, keys) === '';
-    };
-
-    useEffect(() => {
-        console.log(state)
-    }, [state]);
-
-    function onAddButtonClick(fullKey: string[] | string) {
-        const newButton = {
-            "type": "reply",
-            "reply": {
-                "id": "",
-                "title": ""
-            }
-        };
-
-        const currentButtons = getNestedState(fullKey) || [];
-
-        // <set initializer>
-        const cp = initializerRef.current;
-        const currentInitializerButtons = getNestedValue(cp, fullKey) || [];
-        const updatedInitializer = setNestedValue(cp, fullKey, [...currentInitializerButtons, newButton]);
-        initializerRef.current = updatedInitializer;
-        // </set initializer>
-
-        setNestedState(fullKey, [...currentButtons, newButton]);
-    }
-
-    const renderInputs = (messageObj: Record<string, any>, parentKey: string[] = []): (JSX.Element | null)[] => {
-        return Object.keys(messageObj).map((key) => {
-            const fullKey = [...parentKey, key];
-            const value = messageObj[key];
-
-            if (typeof value === 'string' && shouldRenderInput(fullKey)) {
-                return (
-                    <Input
-                        label={formatPlaceholder(fullKey)}
-                        key={fullKey.join('.')}
-                        placeholder={formatPlaceholder(fullKey)}
-                        value={getNestedState(fullKey) || ''}
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => setNestedState(fullKey, e.target.value)}
-                    />
-                );
-            } else if (Array.isArray(value) && key === 'buttons') {
-                return (
-                    <div key={fullKey.join('.')} className="w-full flex flex-col gap-2">
-                        <Label label='Options' />
-                        <div className='ml-5'>
-                            {value.map((button, index) => (
-                                <div key={index} className="flex gap-2 items-center">
-                                    {renderInputs(button, [...fullKey, String(index)])}
-                                </div>
-                            ))}
-                            <Button
-                                text='Add new option'
-                                onClick={() => onAddButtonClick(fullKey)}
-                                className="mt-5 w-full"
-                            />
-                        </div>
-                    </div>
-                );
-            } else if (typeof value === 'object' && value !== null) {
-                return (
-                    <div key={fullKey.join('.')} className="w-full flex flex-col gap-2">
-                        {renderInputs(value, fullKey)}
-                    </div>
-                );
-            } else {
-                return null;
-            }
-        });
-    };
-
-    function formatPlaceholder(keys: string[]): string {
-        return capitalize(keys.slice(-1)[0].replace(/_/g, ' '));
     }
 
     return (
