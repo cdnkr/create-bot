fec4a090319200fd6762ab1847c3b2cd259ec05f
cdnkr <chad@forgood.co.za>
2024-09-04 13:40:34 +0200

    implements nested state hook on add-new WA message form

--- components/create/whatsapp/add-template.tsx
+++ components/create/whatsapp/add-template.tsx
@@ -4,10 +4,11 @@ import Input from '@/components/general/input';
 import Label from '@/components/general/label';
 import Modal from '@/components/general/modal';
 import messageTypeInitializers from '@/data/whatsapp/add-message-initializers';
+import { getNestedValue, setNestedValue, useNestedState } from '@/hooks/useNestedState';
 import { WhatsAppMessageType } from '@/types/whatsapp';
 import { deepClone } from '@/utils/object';
 import { camelCaseToText, capitalize } from '@/utils/text';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 
 interface Props {
     modalTitle: string;
@@ -17,180 +18,139 @@ interface Props {
     setMessages: (val: WhatsAppMessageType[]) => void;
 }
 
-function AddTemplate(props: Props) {
-    const {
-        modalTitle,
-        showModal,
-        setShowModal,
-        messages,
-        setMessages
-    } = props;
-
+function AddTemplate({ modalTitle, showModal, setShowModal, messages, setMessages }: Props) {
     const [templateName, setTemplateName] = useState('');
-    const initialValue = deepClone(messageTypeInitializers.text);
-    const [message, setMessage] = useState<WhatsAppMessageType>({ ...initialValue });
+    const initializerRef = useRef<WhatsAppMessageType>(messageTypeInitializers.text);
+    const { state, getNestedState, setNestedState, resetState } = useNestedState(messageTypeInitializers.text);
 
     function onAddClick() {
-        setMessages([...messages, message]);
+        setMessages([...messages, state]);
         setShowModal(false);
     }
 
     function reInitialize(key: string) {
-        if (!messageTypeInitializers[key]) return;
-
-        const initialValue = deepClone(messageTypeInitializers[key]);
-
-        setMessage({ ...initialValue });
+        if (messageTypeInitializers[key]) {
+            initializerRef.current = deepClone(messageTypeInitializers[key]);
+            resetState(deepClone(messageTypeInitializers[key]));
+        }
     }
 
-
-    const getValue = (key: string) => {
-        const keys = getkeys(key);
-
-        if (Array.isArray(keys)) {
-            if (keys.length == 2) {
-                // @ts-ignore the keys are keysing
-                return message[keys[0]][keys[1]];
-            } else if (keys.length == 3) {
-                // @ts-ignore the keys are keysing
-                return message[keys[0]][keys[1]][keys[2]];
-            }
-        } else {
-            // @ts-ignore the keys are keysing
-            return copy[key];
-        }
+    const shouldRenderInput = (keys: string[]): boolean => {
+        if (!initializerRef.current) return false;
+        // @ts-ignore
+        return getNestedValue(initializerRef.current, keys) === '';
     };
 
-    const setValue = (key: string, value: string) => {
-        let copy = { ...message };
+    useEffect(() => {
+        console.log(state)
+    }, [state]);
 
-        const keys = getkeys(key);
-
-        if ((Array.isArray(keys))) {
-            if (keys.length == 2) {
-                // @ts-ignore the keys are keysing
-                copy[keys[0]][keys[1]] = value;
-            } else if (keys.length == 3) {
-                // @ts-ignore the keys are keysing
-                copy[keys[0]][keys[1]][keys[2]] = value;
+    function onAddButtonClick(fullKey: string[] | string) {
+        const newButton = {
+            "type": "reply",
+            "reply": {
+                "id": "",
+                "title": ""
             }
-        } else {
-            // @ts-ignore the keys are keysing
-            copy[key] = keys;
-        }
+        };
 
-        setMessage({ ...copy });
-    };
+        const currentButtons = getNestedState(fullKey) || [];
 
-    function getkeys(key: string) {
-        if (key.includes('.')) {
-            const keys = key.split('.');
-            return keys;
-        } else {
-            return key;
-        }
+        // <set initializer>
+        const cp = initializerRef.current;
+        const currentInitializerButtons = getNestedValue(cp, fullKey) || [];
+        const updatedInitializer = setNestedValue(cp, fullKey, [...currentInitializerButtons, newButton]);
+        initializerRef.current = updatedInitializer;
+        // </set initializer>
+
+        setNestedState(fullKey, [...currentButtons, newButton]);
     }
 
-    function determineShouldRenderInput(fullKey: string) {
-        const keys = getkeys(fullKey);
-
-        if (Array.isArray(keys)) {
-            if (keys.length == 2) {
-                // @ts-ignore the keys are keysing
-                if (messageTypeInitializers[message.messageKey][keys[0]][keys[1]] === '') return true;
-            } else if (keys.length == 3) {
-                // @ts-ignore the keys are keysing
-                if (messageTypeInitializers[message.messageKey][keys[0]][keys[1]][keys[2]] === '') return true;
-            }
-
-            // @ts-ignore the keys are keysing
-        } else if (messageTypeInitializers[message.messageKey][fullKey] === '') {
-            return true;
-        }
-
-        return false;
-    }
-
-    function formatPlaceholder(str: string) {
-        return capitalize(str.split('.').slice(1).join(' ').replace(/\stext/g, ''));
-    }
-
-    const renderInputs = (messageObj: WhatsAppMessageType, parentKey = ''): (JSX.Element | null)[] => {
+    const renderInputs = (messageObj: Record<string, any>, parentKey: string[] = []): (JSX.Element | null)[] => {
         return Object.keys(messageObj).map((key) => {
-
-            // @ts-ignore the keys are keysing
+            const fullKey = [...parentKey, key];
             const value = messageObj[key];
-            const fullKey = parentKey ? `${parentKey}.${key}` : key;
 
-            const shouldRenderInput = determineShouldRenderInput(fullKey);
-
-            if (typeof value === 'string' && shouldRenderInput) {
+            if (typeof value === 'string' && shouldRenderInput(fullKey)) {
                 return (
                     <Input
                         label={formatPlaceholder(fullKey)}
-                        key={fullKey}
+                        key={fullKey.join('.')}
                         placeholder={formatPlaceholder(fullKey)}
-                        value={getValue(fullKey)}
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => setValue(fullKey, e.target.value)}
+                        value={getNestedState(fullKey) || ''}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => setNestedState(fullKey, e.target.value)}
                     />
                 );
+            } else if (Array.isArray(value) && key === 'buttons') {
+                return (
+                    <div key={fullKey.join('.')} className="w-full flex flex-col gap-2">
+                        <Label label='Options' />
+                        <div className='ml-5'>
+                            {value.map((button, index) => (
+                                <div key={index} className="flex gap-2 items-center">
+                                    {renderInputs(button, [...fullKey, String(index)])}
+                                </div>
+                            ))}
+                            <Button
+                                text='Add new option'
+                                onClick={() => onAddButtonClick(fullKey)}
+                                className="mt-5 w-full"
+                            />
+                        </div>
+                    </div>
+                );
             } else if (typeof value === 'object' && value !== null) {
-                return <div key={fullKey} className='w-full flex flex-col gap-2'>{renderInputs(value, fullKey)}</div>;
+                return (
+                    <div key={fullKey.join('.')} className="w-full flex flex-col gap-2">
+                        {renderInputs(value, fullKey)}
+                    </div>
+                );
             } else {
                 return null;
             }
         });
     };
 
+    function formatPlaceholder(keys: string[]): string {
+        return capitalize(keys.slice(-1)[0].replace(/_/g, ' '));
+    }
+
     return (
-        <Modal
-            showModal={showModal}
-            setShowModal={setShowModal}
-        >
-            <div className='w-full p-8 flex flex-col gap-5'>
-                <h1 className='font-bold text-2xl'>{modalTitle}</h1>
+        <Modal showModal={showModal} setShowModal={setShowModal}>
+            <div className="w-full p-8 flex flex-col gap-5">
+                <h1 className="font-bold text-2xl">{modalTitle}</h1>
                 <Input
-                    label='Template name'
-                    placeholder='E.g. Find out more'
+                    label="Template name"
+                    placeholder="E.g. Find out more"
                     value={templateName}
                     onChange={(e: ChangeEvent<HTMLInputElement>) => setTemplateName(e.target.value)}
                 />
-                <hr className='border-gray-400' />
+                <hr className="border-gray-400" />
                 <div>
-                    <Label label='Select message type' />
-                    <div className='flex flex-wrap gap-y-1 gap-x-5'>
-                        {Object.keys(messageTypeInitializers).map(key => (
-                            <div className={`cursor-pointer`} onClick={() => reInitialize(key)}>
-                                <span className={`text-sm ${(message?.messageKey === key) ? 'text-black underline' : 'text-gray-600'} hover:underline transition-all`}>{camelCaseToText(key)}</span>
+                    <Label label="Select message type" />
+                    <div className="flex flex-wrap gap-y-1 gap-x-5">
+                        {Object.keys(messageTypeInitializers).map((key) => (
+                            <div key={key} className="cursor-pointer" onClick={() => reInitialize(key)}>
+                                <span className={`text-sm ${state?.messageKey === key ? 'text-black underline' : 'text-gray-600'} hover:underline transition-all`}>
+                                    {camelCaseToText(key)}
+                                </span>
                             </div>
                         ))}
                     </div>
                 </div>
-                {/* {(message.type === 'interactive') && (message.interactive.type === 'button') && Array.isArray(message.interactive.action.buttons) && message.interactive.action.buttons.map((button, i) => (
-                    <div className='w-full'>
-                        <label className='text-sm font-bold'>Button {i}</label>
-                        <Input
-                            placeholder='E.g. Find out more'
-                            value={getInteractiveButtonValue(i)}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => setInteractiveButtonValue(i, e.target.value)}
-                        />
+                <div className="flex w-full gap-5">
+                    <div className="flex w-2/3 flex-col gap-2">
+                        {renderInputs({ ...state })}
                     </div>
-                ))} */}
-                <div className='flex w-full gap-5'>
-                    <div className='flex w-2/3 flex-col gap-2'>
-                        {renderInputs(message)}
-                    </div>
-                    <div className='w-1/2'>
-                        <Label label='Preview' />
-                        <Message
-                            message={message}
-                        />
+                    <div className="w-1/2">
+                        <Label label="Preview" />
+                        <Message message={state} />
                     </div>
                 </div>
-                <Button text='Add' onClick={onAddClick} />
+                <Button text="Add" onClick={onAddClick} />
             </div>
         </Modal>
-    )
+    );
 }
 
 export default AddTemplate;
