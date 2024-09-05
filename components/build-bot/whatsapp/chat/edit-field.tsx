22564348b0db9af99a5ce637358ff13746dde63e
Chad Danker <chaddanker@gmail.com>
2024-09-05 23:26:13 +0200

    adds light and dark mode of WA message edit input component

--- components/build-bot/whatsapp/chat/edit-field.tsx
+++ components/build-bot/whatsapp/chat/edit-field.tsx
@@ -1,24 +1,33 @@
+import { stopPropagation } from '@/utils/event';
 import React from 'react'
 
 interface Props {
     value: string;
     onChange: React.ChangeEventHandler<HTMLInputElement>;
     placeholder: string;
+    mode?: 'light' | 'dark'
 }
 
 function EditWhatsAppMessageField(props: Props) {
     const {
         value,
         onChange,
-        placeholder
-    } = props
+        placeholder,
+        mode = 'dark'
+    } = props;
+
+    const MODES = {
+        light: 'text-black',
+        dark: 'text-white'
+    };
 
     return (
         <input
-            className="text-white whitespace-pre-line text-sm mr-2 inline bg-transparent w-auto"
+            className={`${MODES[mode]} whitespace-pre-line text-sm mr-2 inline bg-transparent w-auto`}
             value={value}
             onChange={onChange}
             placeholder={placeholder}
+            onClick={stopPropagation}
         />
     )
 }
