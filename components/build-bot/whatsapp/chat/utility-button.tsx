84ffcb9135bafb47921a2d8281e3962735c2a6fc
Chad Danker <chaddanker@gmail.com>
2024-09-05 23:27:00 +0200

    adds size support for WA utility buttons

--- components/build-bot/whatsapp/chat/utility-button.tsx
+++ components/build-bot/whatsapp/chat/utility-button.tsx
@@ -3,9 +3,10 @@ interface Props {
     Icon?: React.ReactElement;
     label: string;
     type?: 'blueDashed' | 'blueSolid' | 'greenDashed' | 'greenSolid';
+    size?: 'small' | 'normal' 
 }
 
-function WhatsAppUtilityButton({ onClick, Icon, label, type = 'blueDashed' }: Props) {
+function WhatsAppUtilityButton({ onClick, Icon, label, type = 'blueDashed', size = 'normal' }: Props) {
 
     const TYPES = {
         blueDashed: 'text-blue-600 border-dashed border-2 border-blue-500',
@@ -14,12 +15,17 @@ function WhatsAppUtilityButton({ onClick, Icon, label, type = 'blueDashed' }: Pr
         greenSolid: 'text-green-600 border-solid border-2 border-green-500',
     };
 
+    const SIZES = {
+        small: 'p-0',
+        normal: 'p-1'
+    };
+
     return (
         <div
             onClick={onClick}
             className={`flex justify-center min-w-64 items-center rounded-md w-fit mt-2 cursor-pointer bg-transparent mr-auto ${TYPES[type]}`}
         >
-            <div className="flex justify-between break-words items-center gap-2 max-w-[410px] p-2">
+            <div className={`flex justify-between break-words items-center gap-2 max-w-[410px] ${SIZES[size]}`}>
                 {Icon}
                 {label}
             </div>
