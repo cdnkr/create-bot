0cc06d4ff6c18414e1caef31cf55a57701311c54
cdnkr <chad@forgood.co.za>
2024-09-06 09:28:43 +0200

    adds additional className for WA utility button

--- components/build-bot/whatsapp/chat/utility-button.tsx
+++ components/build-bot/whatsapp/chat/utility-button.tsx
@@ -2,11 +2,12 @@ interface Props {
     onClick?: () => void;
     Icon?: React.ReactElement;
     label: string;
+    className?: string;
     type?: 'blueDashed' | 'blueSolid' | 'greenDashed' | 'greenSolid';
     size?: 'small' | 'normal' 
 }
 
-function WhatsAppUtilityButton({ onClick, Icon, label, type = 'blueDashed', size = 'normal' }: Props) {
+function WhatsAppUtilityButton({ onClick, Icon, label, className = '', type = 'blueDashed', size = 'normal' }: Props) {
 
     const TYPES = {
         blueDashed: 'text-blue-600 border-dashed border-2 border-blue-500',
@@ -23,7 +24,7 @@ function WhatsAppUtilityButton({ onClick, Icon, label, type = 'blueDashed', size
     return (
         <div
             onClick={onClick}
-            className={`flex justify-center min-w-64 items-center rounded-md w-fit mt-2 cursor-pointer bg-transparent mr-auto ${TYPES[type]}`}
+            className={`flex justify-center min-w-64 items-center rounded-md w-fit mt-2 cursor-pointer bg-transparent mr-auto ${className} ${TYPES[type]}`}
         >
             <div className={`flex justify-between break-words items-center gap-2 max-w-[410px] ${SIZES[size]}`}>
                 {Icon}
