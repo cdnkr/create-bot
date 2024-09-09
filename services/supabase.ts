816f6912309b48f8ec3f30a3c45d1e8601783103
Chad Danker <chaddanker@gmail.com>
2024-09-10 00:16:00 +0200

    adds supabase initialization

---
+++ services/supabase.ts
@@ -0,0 +1,9 @@
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl as string, supabaseKey as string);
+
+export default supabase;
+
