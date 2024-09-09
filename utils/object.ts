df445fbedadb4dcd733ffdf597043cef36bf8b81
Chad Danker <chaddanker@gmail.com>
2024-09-10 00:12:53 +0200

    adds util for empty object checking

--- utils/object.ts
+++ utils/object.ts
@@ -1,3 +1,7 @@
 export function deepClone(obj: any) {
     return JSON.parse(JSON.stringify(obj));
 }
+
+export function isEmptyObject(obj: any) {
+    return Boolean(Object.keys(obj).length === 0);
+}
