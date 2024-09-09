739952fce7fd97df116079435cb53bfc96ece504
Chad Danker <chaddanker@gmail.com>
2024-09-10 00:19:52 +0200

    saves flow in supabase db

--- app/api/flow/save/route.ts
+++ app/api/flow/save/route.ts
@@ -1,27 +1,51 @@
-import { NextResponse } from 'next/server'
-import { promises as fs } from 'fs'
-import path from 'path'
+import supabase from '@/services/supabase';
+import { NextResponse } from 'next/server';
+import { v4 } from 'uuid';
 
 export async function POST(request: Request) {
     try {
         // Parse the request body
         const body = await request.json();
-        const flowId = body.id;
+        const userId = '5ab6b5e3-5cbe-48dd-8714-b97d5f090cc2';
 
-        if (!flowId) {
-            return NextResponse.json({ error: 'flowId is required in the body' }, { status: 400 });
-        }
+        const newFlowId = v4();
+        
+        const { error } = await supabase
+        .from('flows')
+        .insert({
+            id: newFlowId,
+            user_id: userId, 
+            templates: body.templates, 
+            wa_number: body.waNumber,
+            wa_access_token: body.waAccessToken
+        });
 
-        // Define the path for the JSON file based on flowId
-        const filePath = path.join(process.cwd(), 'tmp', 'flows', `${flowId}.json`);
+        if (error) return NextResponse.json({ error }, { status: 500 });
 
-        // Create the 'flows' directory if it doesn't exist
-        await fs.mkdir(path.dirname(filePath), { recursive: true });
+        return NextResponse.json({ msg: `Flow saved`, id: newFlowId });
+    } catch (error) {
+        console.error('Error saving data:', error);
+        return NextResponse.json({ error: 'Failed to save data' }, { status: 500 });
+    }
+}
 
-        // Write the body to the JSON file
-        await fs.writeFile(filePath, JSON.stringify(body, null, 2));
+export async function PUT(request: Request) {
+    try {
+        // Parse the request body
+        const body = await request.json();
+        
+        const { error } = await supabase
+        .from('flows')
+        .update({
+            templates: body.templates, 
+            wa_number: body.waNumber,
+            wa_access_token: body.waAccessToken
+        })
+        .eq('id', body.id);
 
-        return NextResponse.json({ msg: `Data saved to ${flowId}.json` });
+        if (error) return NextResponse.json({ error }, { status: 500 });
+
+        return NextResponse.json({ msg: `Flow saved`, id: body.id });
     } catch (error) {
         console.error('Error saving data:', error);
         return NextResponse.json({ error: 'Failed to save data' }, { status: 500 });
