98604977a2c70bf0e736cbe5e0121d90901f6f33
Chad Danker <chaddanker@gmail.com>
2024-09-10 21:48:10 +0200

    adds icon support to button component

--- components/general/button.tsx
+++ components/general/button.tsx
@@ -5,20 +5,25 @@ interface Props {
     text: string;
     className?: string;
     disabled?: boolean;
+    Icon?: React.ReactElement
 }
 
-function Button({ 
+function Button({
     onClick,
     text,
     className = '',
     disabled = false,
+    Icon
 }: Props) {
     return (
         <button
             onClick={onClick}
-            className={`text-white shrink-0 flex-nowrap text-nowrap bg-black focus:ring-orange-300 ${className} hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ${disabled ? 'opacity-50' : ''}`}
+            className={`text-white shrink-0 flex justify-center items-center gap-2 flex-nowrap text-nowrap bg-black focus:ring-orange-300 ${className} hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ${disabled ? 'opacity-50' : ''}`}
             disabled={disabled}
         >
+            <div className='text-lg'>
+                {Icon}
+            </div>
             {text}
         </button>
     )
