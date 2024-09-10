fb562223c14ac5150ab3fb8beaa4e1d7ca3b99e8
Chad Danker <chaddanker@gmail.com>
2024-09-11 00:32:34 +0200

    change button size and fix margin

--- components/general/button.tsx
+++ components/general/button.tsx
@@ -18,7 +18,7 @@ function Button({
     return (
         <button
             onClick={onClick}
-            className={`text-white shrink-0 flex justify-center items-center gap-2 flex-nowrap text-nowrap bg-black focus:ring-orange-300 ${className} hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ${disabled ? 'opacity-50' : ''}`}
+            className={`text-white shrink-0 flex justify-center items-center gap-2 flex-nowrap text-nowrap bg-black focus:ring-orange-300 ${className} hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-3.5 text-center ${disabled ? 'opacity-50' : ''}`}
             disabled={disabled}
         >
             <div className='text-lg'>
