3dcd2dfd867eb78cc206cfc20719666d1fe69e7a
Chad Danker <chaddanker@gmail.com>
2024-09-11 00:20:58 +0200

    adds loading spinner

---
+++ components/general/loading-spinner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoadingSpinner() {
+    return (
+        <div className="border-transparent h-5 w-5 animate-spin rounded-full border border-t-green-500 border-r-green-500" />
+    )
+}
+
+export default LoadingSpinner;
