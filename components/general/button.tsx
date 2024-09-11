34f6b75eeebb1555c91429752b8cfaddc669b3ff
Chad Danker <chaddanker@gmail.com>
2024-09-12 00:16:33 +0200

    adds multiple color support and icon placement on button

--- components/general/button.tsx
+++ components/general/button.tsx
@@ -5,7 +5,10 @@ interface Props {
     text: string;
     className?: string;
     disabled?: boolean;
-    Icon?: React.ReactElement
+    Icon?: React.ReactElement;
+    iconEnd?: boolean;
+    color?: 'white' | 'black' | 'blue';
+    hoverScale?: boolean;
 }
 
 function Button({
@@ -13,18 +16,34 @@ function Button({
     text,
     className = '',
     disabled = false,
-    Icon
+    Icon,
+    iconEnd = false,
+    color = 'black',
+    hoverScale = true
 }: Props) {
+    const COLOR_STYLES = {
+        black: 'text-white bg-black',
+        white: 'text-black bg-white',
+        blue: 'text-white bg-blue-600'
+    };
+
     return (
         <button
             onClick={onClick}
-            className={`text-white shrink-0 flex justify-center items-center gap-2 flex-nowrap text-nowrap bg-black focus:ring-orange-300 ${className} hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-3.5 text-center ${disabled ? 'opacity-50' : ''}`}
+            className={`${COLOR_STYLES[color]} ${hoverScale ? 'hover:scale-105' : ''} shrink-0 flex justify-center items-center gap-2 flex-nowrap text-nowrap focus:ring-blue-300 ${className} hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-3.5 text-center ${disabled ? 'opacity-50' : ''}`}
             disabled={disabled}
         >
-            <div className='text-lg'>
-                {Icon}
-            </div>
+            {!iconEnd && (
+                <div className='text-lg'>
+                    {Icon}
+                </div>
+            )}
             {text}
+            {iconEnd && (
+                <div className='text-lg'>
+                    {Icon}
+                </div>
+            )}
         </button>
     )
 }
