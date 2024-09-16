5cbbbed43c7cc3124ec0e17ec489745dd1279476
Chad Danker <chaddanker@gmail.com>
2024-09-16 22:29:40 +0200

    adds customizable bg color on modal

--- components/general/modal.tsx
+++ components/general/modal.tsx
@@ -6,12 +6,14 @@ interface Props {
     showModal: boolean;
     setShowModal: (val: boolean) => void;
     children: React.ReactNode;
+    bgColor?: string;
 }
 
 export default function Modal({
     showModal,
     setShowModal,
-    children
+    children,
+    bgColor = 'bg-slate-100'
 }: Props) {
     return (
         <Dialog open={showModal} onClose={() => setShowModal(false)} className='relative z-20'>
@@ -24,7 +26,7 @@ export default function Modal({
                 <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
                     <DialogPanel
                         transition
-                        className='relative transform overflow-hidden md:min-w-[700px] rounded-lg bg-slate-100 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95'
+                        className={`relative transform overflow-hidden rounded-lg ${bgColor} text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95`}
                     >
                         {children}
                     </DialogPanel>
