d1fb5d97580653369e67f70da93ef1913310acb3
Chad Danker <chaddanker@gmail.com>
2024-09-16 22:26:25 +0200

    adds component to display an html string

---
+++ components/html/display-html-string.tsx
@@ -0,0 +1,16 @@
+interface Props {
+  content: string;
+}
+
+const DisplayHtmlString = ({
+  content
+}: Props) => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: content }}
+      className="prose prose-hr:my-1 prose-p:font-light prose-li:font-light prose-p:m-0 prose-li:m-0 my-5 w-full max-w-full"
+    />
+  );
+};
+
+export default DisplayHtmlString;
