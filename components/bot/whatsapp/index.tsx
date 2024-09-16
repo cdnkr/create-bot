06fced394eeb9b87350a77c548a54f71dd1aa732
Chad Danker <chaddanker@gmail.com>
2024-09-16 22:36:13 +0200

    pass user object to wa bot builder component

--- components/bot/whatsapp/index.tsx
+++ components/bot/whatsapp/index.tsx
@@ -1,18 +1,21 @@
 'use client'
 
+import { User } from "@supabase/supabase-js";
 import WhatsAppChat from "./chat";
 
 interface Props {
   botDetails?: any;
+  user: User | null;
 }
 
 export default function BuildWhatsAppBot({
-  botDetails
+  botDetails,
+  user
 }: Props) {
   return (
     <div className="w-full flex flex-col md:flex-row gap-5">
       <div className="w-full">
-        <WhatsAppChat botDetails={botDetails} />
+        <WhatsAppChat botDetails={botDetails} user={user} />
       </div>
     </div>
   );
